0d256b7cc5e27b476b014439ab7e7da2e9c6b64e
Javier Estupinan <jestupinan@zeerbyte.com>
2020-05-23 11:00:16 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  entry: './src/js/app.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: process.env.NODE_ENV === 'production' ? null : 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    writeToDisk: true
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/public' }
+      ]
+    })
+  ]
+};
